52b966b60134d6d3420a0242b398f147fa04c5a7
gloriafercu <gloriafercu@gmail.com>
2018-05-23 22:38:02 +0200

    Prueba

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 export default function application() {
 
-var result;
+	var result;
 
 	function romanizerNumerals(number) {
 		var numerals = [
-			{ value: 1000, roman: 'M' },
-			{ value: 900, roman: 'CM' },
-			{ value: 500, roman: 'D' },
-			{ value: 400, roman: 'CD' },
-			{ value: 100, roman: 'C' },
-			{ value: 90, roman: 'XC' },
-			{ value: 50, roman: 'L' },
-			{ value: 40, roman: 'XL' },
-			{ value: 10, roman: 'X' },
-			{ value: 9, roman: 'IX' },
-			{ value: 5, roman: 'V' },
-			{ value: 4, roman: 'IV' },
-			{ value: 1, roman: 'I' }
+			{ value: 1000, roman: 'M'  },
+			{ value: 900,  roman: 'CM' },
+			{ value: 500,  roman: 'D'  },
+			{ value: 400,  roman: 'CD' },
+			{ value: 100,  roman: 'C'  },
+			{ value: 90,   roman: 'XC' },
+			{ value: 50,   roman: 'L'  },
+			{ value: 40,   roman: 'XL' },
+			{ value: 10,   roman: 'X'  },
+			{ value: 9,    roman: 'IX' },
+			{ value: 5,    roman: 'V'  },
+			{ value: 4,    roman: 'IV' },
+			{ value: 1,    roman: 'I'  }
 		];
 		var remaining = number;
 		var result = '';
